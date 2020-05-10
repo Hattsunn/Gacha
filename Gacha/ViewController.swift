94dbceced5ede75ac1b7481d756095a17d91ad47
okada hatsune <swan.nilda1211@gmail.com>
2020-05-10 10:13:18 +0900

    Initial Commit

---
+++ Gacha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gacha
+//
+//  Created by Hatsune Okada on 5/10/20.
+//  Copyright © 2020 Okada Hattsunn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
